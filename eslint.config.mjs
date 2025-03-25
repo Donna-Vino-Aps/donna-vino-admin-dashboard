c0a15c9a805a6712d8406b1b8cfb44a73e3b3cbf
lenin del rio <lenindelrionicaragua@gmail.com>
2025-03-25 13:51:33 +0100

    feat: add eslint.config.msj

--- eslint.config.mjs
+++ eslint.config.mjs
@@ -1,14 +1,68 @@
-import { dirname } from "path";
-import { fileURLToPath } from "url";
-import { FlatCompat } from "@eslint/eslintrc";
+const { defineConfig } = require("eslint-define-config");
+const eslintPluginReact = require("eslint-plugin-react");
+const eslintPluginJsxA11y = require("eslint-plugin-jsx-a11y");
+const eslintPluginReactHooks = require("eslint-plugin-react-hooks");
+const eslintPluginCypress = require("eslint-plugin-cypress");
 
-const __filename = fileURLToPath(import.meta.url);
-const __dirname = dirname(__filename);
-
-const compat = new FlatCompat({
-  baseDirectory: __dirname,
-});
-
-const eslintConfig = [...compat.extends("next/core-web-vitals")];
-
-export default eslintConfig;
+module.exports = defineConfig([
+  {
+    ignores: ["**/.next/**"],
+    languageOptions: {
+      globals: {
+        "cypress/globals": true,
+        browser: true,
+        es2021: true,
+        jest: true,
+      },
+      parserOptions: {
+        ecmaVersion: 2021,
+        sourceType: "module",
+        ecmaFeatures: {
+          jsx: true,
+        },
+      },
+    },
+    plugins: {
+      react: eslintPluginReact,
+      "jsx-a11y": eslintPluginJsxA11y,
+      "react-hooks": eslintPluginReactHooks,
+      cypress: eslintPluginCypress,
+    },
+    settings: {
+      react: {
+        version: "detect",
+      },
+    },
+    rules: {
+      "react/prop-types": ["warn"],
+      "no-unused-vars": ["warn", { varsIgnorePattern: "^React$" }],
+      "react/jsx-uses-react": "off",
+      "react/jsx-uses-vars": "error",
+      "no-console": ["error", { allow: ["warn", "error"] }],
+    },
+  },
+  {
+    // Specific configuration for .cy.js files
+    files: ["**/*.cy.js", "cypress.config.js"],
+    languageOptions: {
+      globals: {
+        "cypress/globals": true,
+      },
+    },
+    plugins: {
+      cypress: eslintPluginCypress,
+    },
+    rules: {
+      "no-unused-vars": "off",
+      "no-console": "off",
+      "cypress/no-assertion-after-screenshot": "off",
+    },
+  },
+  {
+    // Specific configuration for logging.js
+    files: ["src/utils/logging.js"],
+    rules: {
+      "no-console": "off",
+    },
+  },
+]);

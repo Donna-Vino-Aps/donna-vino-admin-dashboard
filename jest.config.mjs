6a810a956ab2c7ef49d8e244f2d676c12937e4ac
lenin del rio <lenindelrionicaragua@gmail.com>
2025-03-25 13:52:56 +0100

    feat: add feat.config.js

---
+++ jest.config.mjs
@@ -0,0 +1,19 @@
+export default {
+  preset: "./jest-preset.mjs",
+  testEnvironment: "jest-environment-jsdom",
+  transform: {
+    "^.+\\.(js|jsx|mjs)$": "babel-jest",
+    "^.+\\.(ts|tsx)$": "babel-jest",
+  },
+  transformIgnorePatterns: ["/node_modules/(?!(swiper|ssr-window|dom7)/)"],
+  moduleFileExtensions: ["js", "jsx", "mjs", "json", "node"],
+  moduleNameMapper: {
+    "^@/(.*)$": "<rootDir>/src/$1",
+    // Map Swiper CSS imports to the style mock
+    "^swiper\\/css$": "<rootDir>/src/__mock__/styleMock.js",
+    "^swiper\\/css\\/.*$": "<rootDir>/src/__mock__/styleMock.js",
+    "\\.(css|scss|sass)$": "<rootDir>/src/__mock__/styleMock.js",
+  },
+  setupFilesAfterEnv: ["<rootDir>/setupTests.js"],
+  collectCoverageFrom: ["src/**/*.{js,jsx,ts,tsx}"],
+};

dd21a8ca77ea0dc9295f7475b418550d2929e6b3
lenin del rio <lenindelrionicaragua@gmail.com>
2025-03-28 10:51:34 +0100

    feat: create LanguageSwitch.test.js

--- src/components/Footer/Footer.js
+++ src/components/Footer/Footer.js
@@ -2,47 +2,8 @@
 
 import React from "react";
 import Link from "next/link";
-import { useLanguage } from "../../context/LanguageContext";
 
 const Footer = () => {
-  const { translations } = useLanguage();
-
-  const links = [
-    {
-      href: "/our-team",
-      label: translations["footer.team"],
-      dataTestId: "our-team",
-    },
-    {
-      href: "/our-values",
-      label: translations["footer.values"],
-      dataTestId: "our-values",
-    },
-    {
-      href: "/contact",
-      label: translations["footer.contact"],
-      dataTestId: "contact",
-    },
-  ];
-
-  const socialLinks = [
-    {
-      href: "https://www.facebook.com/donnavino.dk/",
-      src: "/icons/footer/facebook-line.svg",
-      alt: "Facebook Logo",
-    },
-    {
-      href: "https://www.instagram.com/donna_vino_winetastings/",
-      src: "/icons/footer/instagram-original.svg",
-      alt: "Instagram Logo",
-    },
-    {
-      href: "https://www.linkedin.com/company/donna-vino-aps/",
-      src: "/icons/footer/linkedin-alt.svg",
-      alt: "LinkedIn Logo",
-    },
-  ];
-
   return (
     <footer
       className="flex text-white text-center py-4 h-96 bg-[#2F2E2E] md:h-[26.625rem] items-center justify-center"
@@ -59,42 +20,50 @@ const Footer = () => {
           />
         </Link>
 
-        {links.map(({ href, label, dataTestId }, index) => (
-          <Link
-            key={index}
-            data-testid={dataTestId}
-            href={href}
-            className={`rounded-md px-3 py-2 text-bodyLarge text-semibold order-${index + 2} md:order-${index + 1}`}
-            role="navigation"
-            aria-label={`Link to ${label}`}
-          >
-            {label}
-          </Link>
-        ))}
-
         <div className="flex flex-col order-1 md:order-4 items-center md:relative md:top-5">
           <h4 className="text-bodyLarge text-semibold mb-1 md:mb-3 md:mt-3">
-            {translations["footer.follow"]}
+            Follow Us
           </h4>
           <div
             className="flex gap-4 justify-center mt-3 mb-1"
             aria-label="Social media icons"
           >
-            {socialLinks.map(({ href, src, alt }, index) => (
-              <a
-                key={index}
-                href={href}
-                target="_blank"
-                rel="noopener noreferrer"
-                className="text-white"
-              >
-                <img
-                  src={src}
-                  alt={alt}
-                  className="h-[1.5rem] w-[1.5rem] filter brightness-0 invert"
-                />
-              </a>
-            ))}
+            <a
+              href="https://www.facebook.com/donnavino.dk/"
+              target="_blank"
+              rel="noopener noreferrer"
+              className="text-white"
+            >
+              <img
+                src="/icons/footer/facebook-line.svg"
+                alt="Facebook Logo"
+                className="h-[1.5rem] w-[1.5rem] filter brightness-0 invert"
+              />
+            </a>
+            <a
+              href="https://www.instagram.com/donna_vino_winetastings/"
+              target="_blank"
+              rel="noopener noreferrer"
+              className="text-white"
+            >
+              <img
+                src="/icons/footer/instagram-original.svg"
+                alt="Instagram Logo"
+                className="h-[1.5rem] w-[1.5rem] filter brightness-0 invert"
+              />
+            </a>
+            <a
+              href="https://www.linkedin.com/company/donna-vino-aps/"
+              target="_blank"
+              rel="noopener noreferrer"
+              className="text-white"
+            >
+              <img
+                src="/icons/footer/linkedin-alt.svg"
+                alt="LinkedIn Logo"
+                className="h-[1.5rem] w-[1.5rem] filter brightness-0 invert"
+              />
+            </a>
           </div>
         </div>
       </div>
